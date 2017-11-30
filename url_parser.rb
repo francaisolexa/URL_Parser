be557fe38b48374eb0c39871f58b76872e6bbadf
francaisolexa <folexa@email.lynn.edu>
2017-11-30 08:32:54 -0500

    query_string method no longer returns error; variable for port was reassigned to prevent error

--- url_parser.rb
+++ url_parser.rb
@@ -2,9 +2,6 @@ class UrlParser
   def initialize(url)
     @url = url
     @after_scheme = @url.split('://')[1]
-    @after_domain = @after_scheme.split(':')[1]
-    @after_port = @after_domain.split('/')[1]
-    @after_path = @after_port.split('?')[1]
   end
 
   def scheme
@@ -20,36 +17,38 @@ class UrlParser
   end
 
   def port
-    @after_domain.split('/')[0]
+    if @after_scheme.include? ":"
+      prt = @after_scheme.split(':')[1]
+    end
+    if prt.include? "/"
+      prt = prt.split('/')[0]
+    end
   end
 
   def path
-    if @after_port.include? '?'
-      this_path = @after_port.split('?')[0]
-    elsif @after_port.include? '#'
-      this_path = @after_port.split('#')[0]
+    if @after_scheme.include? '?'
+      pth = @after_scheme.split('?')[0]
+    end
+    pth.split('/')[1]
+  end
+
+  def query_string
+    if @after_scheme.include? '?'
+      qry = @after_scheme.split('?')[1]
+    end
+    if qry.include? '#'
+      qry = qry.split('#')[0]
     end
 
-
-    if this_path == ""
-      return nil
-      
-
-  end
-
-def query_string
-  query_hash = {}
-  if @after_path.include? '#'
-    qry = @after_path.split('#')[0]
-  end
-  qry_array = qry.split("&")
-  qry_array.each do |el|
+    query_hash = {}
+    qry_array = qry.split("&")
+    qry_array.each do |el|
     query1 = el.split("=")[0]
     query2 = el.split("=")[1]
     query_hash[query1] = query2
+    end
+    query_hash
   end
-  query_hash
-end
 
   def fragment_id
     @url.split('#').last
@@ -57,5 +56,5 @@ end
 end
 
 @url = UrlParser.new('http://www.google.com:60/search?q=cat&name=Tim#img=FunnyCat')
-# @url = UrlParser.new('https://www.google.com/?q=cat#img=FunnyCat')
-end
+
+p @url
